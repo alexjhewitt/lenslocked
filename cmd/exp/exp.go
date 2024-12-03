14e6d5d9391f0b895e3b30ecbafaa950ce759a7b
Alex <alexhewitt.j@gmail.com>
2024-12-03 07:29:24 -0600

    add in template example

---
+++ cmd/exp/exp.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Bio  string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	user := User{
+		Name: "John Smith",
+		Bio:  `<script>alert("Haha, you've been hacked!");</script>`,
+	}
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
