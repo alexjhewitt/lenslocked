1e983a1f8990ea7cb2d16619596f6d4d2d05d50a
Alex <alexhewitt.j@gmail.com>
2024-12-03 08:09:22 -0600

    add in more types and unpack

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -7,10 +7,12 @@ import (
 )
 
 type User struct {
-	Name string
-	Bio  string
-	Age  int
-	Time time.Time
+	Name            string
+	Bio             string
+	Age             int
+	Time            time.Time
+	FavoriteThings  map[string]string
+	FavoriteNumbers []int
 }
 
 func main() {
@@ -19,10 +21,12 @@ func main() {
 		panic(err)
 	}
 	user := User{
-		Name: "John Smith",
-		Bio:  `<script>alert("Haha, you've been hacked!");</script>`,
-		Age:  27,
-		Time: time.Now(),
+		Name:            "John Smith",
+		Bio:             `<script>alert("Haha, you've been hacked!");</script>`,
+		Age:             27,
+		Time:            time.Now(),
+		FavoriteThings:  map[string]string{"animal": "dog", "food": "pizza"},
+		FavoriteNumbers: []int{5, 10, 15, 20},
 	}
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
