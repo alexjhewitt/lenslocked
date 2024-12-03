77bb547ffbfc93278b4c1b0c16e0ee2b384d0448
Alex Hewitt <alexhewitt.j@gmail.com>
2024-12-03 14:06:16 -0600

    add middleware

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,11 +23,19 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>FAQ</h1><p><ul><li>Name: Alex Hewitt</li><li>Occupation: Software developer</li><li>Learning: Golang</li></ul></p>")
 }
 
+func faqPersonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	person := chi.URLParam(r, "person")
+	fmt.Fprintf(w, "<h1>Hello, %s!</h1>", person)
+}
+
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/faq/{person}", faqPersonHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
