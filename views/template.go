46afd0446080fab78e1e9684664a7b0b89bdf6c4
Alex <alexhewitt.j@gmail.com>
2024-12-10 12:17:30 -0600

    add must func

--- views/template.go
+++ views/template.go
@@ -7,18 +7,11 @@ import (
 	"net/http"
 )
 
-type Template struct {
-	htmlTpl *template.Template
-}
-
-func (t Template) Execute(w http.ResponseWriter, data interface {}) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err := t.htmlTpl.Execute(w, data)
+func Must(t Template, err error) Template {
 	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
+		panic(err)
 	}
+	return t
 }
 
 func Parse(filepath string) (Template, error) {
@@ -29,4 +22,18 @@ func Parse(filepath string) (Template, error) {
 	return Template{
 		htmlTpl: htmlTpl,
 	}, nil
+}
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.htmlTpl.Execute(w, data)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
